d7e9d9a97196ef03d43ddd8eefb46bbea5d64f59
svvhuang <svveetiehuang@163.com>
2019-10-30 10:46:16 +0800

    add antd-BackTop

---
+++ src/pages/post/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+// import { connect } from 'react-redux';
+import { Wrapper } from './style';
+import { BackTop } from 'antd';
+
+// const Home = (props) => {
+const Home: React.FC = (props) => {
+	return (
+		<Wrapper>
+		
+			{/* <BackTop> */}
+		</Wrapper>
+	);
+};
+
+export default Home;
