b272c39a18d3ce07248c17c18d93ca13f3933dad
anthhub <anthhub@163.com>
2019-10-30 18:01:01 +0800

    dir change

--- src/routes/lazyComponents.tsx
+++ src/routes/lazyComponents.tsx
@@ -1,21 +1,21 @@
-import Frame from '@/containers/Frame';
+import Frame from '@/containers/Frame'
 
-import { Spin } from 'antd';
-import React, { lazy, Suspense } from 'react';
+import { Spin } from 'antd'
+import React, { lazy, Suspense } from 'react'
 
-export type TLazyComponentsKeys = keyof typeof lazyComponents;
+export type TLazyComponentsKeys = keyof typeof lazyComponents
 
 const withSuspense = (Component: any) => {
-	return (props: any) => (
-		<Suspense fallback={<Spin />}>
-			<Component {...props} />
-		</Suspense>
-	);
-};
+  return (props: any) => (
+    <Suspense fallback={<Spin />}>
+      <Component {...props} />
+    </Suspense>
+  )
+}
 
 export const lazyComponents = {
-	Frame,
-	Home: withSuspense(lazy(() => import('../pages/home'))),
-	Post: withSuspense(lazy(() => import('../pages/post'))),
-	Test: withSuspense(lazy(() => import('../pages/test')))
-};
+  Frame,
+  Home: withSuspense(lazy(() => import('../pages/home'))),
+  Post: withSuspense(lazy(() => import('../pages/post'))),
+  Test: withSuspense(lazy(() => import('../pages/test'))),
+}
