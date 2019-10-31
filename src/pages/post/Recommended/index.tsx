163728c988bfb922c564c0ca023e30f0d209e47b
svvhuang <svveetiehuang@163.com>
2019-10-31 10:10:25 +0800

    delete Recommended

--- src/pages/post/Recommended/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-// import { connect } from 'react-redux';
-import { Wrapper } from './style';
-
-const Recommended: React.FC = (props) => {
-	return <Wrapper />;
-};
-
-export default Recommended;
