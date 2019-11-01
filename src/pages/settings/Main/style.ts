dec8c717c3e0d0de86ec4fb69b3f59a2bbe01e9b
svvhuang <svveetiehuang@163.com>
2019-11-01 11:16:34 +0800

    add settings/profile

--- src/pages/settings/Main/style.ts
+++ src/pages/settings/Main/style.ts
@@ -6,6 +6,7 @@
 
 import styled from 'styled-components';
 import avatarPic from '../../../statics/avatar.png';
+import editIcon from '../../../statics/edit.svg';
 
 export const Wrapper = styled.div`
 	width: 696px;
@@ -39,15 +40,96 @@ export const Wrapper = styled.div`
 		.item-title {
 			/* display:block; */
 			width: 120px;
+			font-size: 14px;
 		}
 
 		.avatar-uploader {
 			display: flex;
+
 			.avatar {
 				width: 72px;
 				height: 72px;
 				margin-right: 12px;
 				background: #eee url(${avatarPic}) no-repeat center/cover;
+			}
+
+			.upload {
+				margin-left: 12px;
+
+				.hint {
+					margin-bottom: 18px;
+					color: #909090;
+					font-size: 12px;
+				}
+
+				.upload-btn {
+					padding: 6px 16px;
+					color: #fff;
+					background: #007fff;
+					border-radius: 2px;
+					border: none;
+					outline: none;
+					cursor: pointer;
+				}
+			}
+		}
+
+		.input-box {
+			display: flex;
+			justify-content: space-between;
+			flex: 1;
+
+			.input {
+				flex: 1;
+				font-size: 16px;
+				color: #909090;
+				border: none;
+				outline: none;
+
+				// 兼容不同浏览器的 placeholder
+				::-webkit-input-placeholder {
+					color: #ccc;
+				}
+
+				:-moz-placeholder {
+					color: #ccc;
+				}
+
+				::-moz-placeholder {
+					color: #ccc;
+				}
+
+				:-ms-input-placeholder {
+					color: #ccc;
+				}
+			}
+
+			.edit-box {
+				margin-left: 12px;
+
+				.edit-btn,
+				.confirm-btn,
+				.cancel-btn {
+					font-size: 14px;
+					color: #007fff;
+					border: none;
+					background: #fff;
+					outline: none;
+					cursor: pointer;
+
+					.edit-icon {
+						display: inline-block;
+						width: 18px;
+						height: 18px;
+						margin-right: 7px;
+						background: url(${editIcon}) no-repeat center/contain;
+						vertical-align: bottom;
+					}
+				}
+
+				.cancel-btn {
+					color: #666;
+				}
 			}
 		}
 	}
