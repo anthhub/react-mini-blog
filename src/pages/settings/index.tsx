782bc24557306ed40038888ac131621323e2698a
svvhuang <svveetiehuang@163.com>
2019-11-01 11:51:47 +0800

    use function

--- src/pages/settings/index.tsx
+++ src/pages/settings/index.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import Navigation from './Navigation';
-import Main from './Main';
+import Profile from './Profile';
 import { Wrapper } from './style';
+
+// 被选中的导航栏标签序号（从 0 开始）
+const activeTag: number = 0;
+
+const getView = (tag: number) => {
+	switch (tag) {
+		case 0:
+			return <Profile />;
+		case 1:
+			return <Profile />;
+	}
+};
 
 const Settings: React.FC = (props) => {
 	return (
 		<Wrapper>
 			<Navigation />
-			<Main />
+			{getView(activeTag)}
 		</Wrapper>
 	);
 };
