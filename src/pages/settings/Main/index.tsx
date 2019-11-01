e1c1a58704c4b227731ba0a1e3fa7d1e800277ce
svvhuang <svveetiehuang@163.com>
2019-11-01 11:16:34 +0800

    add settings/profile

--- src/pages/settings/Main/index.tsx
+++ src/pages/settings/Main/index.tsx
@@ -1,30 +1,86 @@
-import React from 'react'
+import React from 'react';
 // import { connect } from 'react-redux';
-import { Wrapper } from './style'
+import { Wrapper } from './style';
 
-const Main: React.FC = props => {
-  return <Wrapper>
-<div>
-  <h1 className='title'>个人资料
-</h1>
-  <ul className='setting-list'>
-  <li className='item'>
-    <span className='item-title'>头像</span>
-    <div className='avatar-uploader'>
-      <div className='avatar'/>
-      <div>
-        <div>支持 jpg、png 格式大小 5M 以内的图片</div>
-      </div>
-      <button>点击上传</button>
-    </div>
-  </li>
-  <li className='item'><span  className='item-title'>用户名</span><div></div></li>
-  <li className='item'><span className='item-title'>邮箱</span><div></div></li>
-  <li className='item'><span className='item-title'>GitHub</span><div></div></li>
-  </ul>
-  </div>
+// const editStatus: boolean = true;
+const editStatus: boolean = false;
+const Main: React.FC = (props) => {
+	return (
+		<Wrapper>
+			<div>
+				<h1 className="title">个人资料</h1>
+				<ul className="setting-list">
+					<li className="item">
+						<span className="item-title">头像</span>
+						<div className="avatar-uploader">
+							<div className="avatar" />
+							<div className="upload">
+								<div className="hint">支持 jpg、png 格式大小 5M 以内的图片</div>
+								<button className="upload-btn">点击上传</button>
+							</div>
+						</div>
+					</li>
+					<li className="item">
+						<span className="item-title">用户名</span>
+						<div className="input-box">
+							<input className="input" placeholder="填写你的用户名" />
+							{editStatus ? (
+								<div className="edit-box">
+									<button className="edit-btn">
+										<i className="edit-icon" />
+										<span>修改</span>
+									</button>
+								</div>
+							) : (
+								<div className="edit-box">
+									<button className="confirm-btn">保存</button>
+									<button className="cancel-btn">取消</button>
+								</div>
+							)}
+						</div>
+					</li>
+					<li className="item">
+						<span className="item-title">邮箱</span>
+						<div className="input-box">
+							<input className="input" placeholder="填写你的邮箱" />
+							{editStatus ? (
+								<div className="edit-box">
+									<button className="edit-btn">
+										<i className="edit-icon" />
+										<span>修改</span>
+									</button>
+								</div>
+							) : (
+								<div className="edit-box">
+									<button className="confirm-btn">保存</button>
+									<button className="cancel-btn">取消</button>
+								</div>
+							)}
+						</div>
+					</li>
+					<li className="item">
+						<span className="item-title">GitHub</span>
+						<div className="input-box">
+							<input className="input" placeholder="填写你的 GitHub" />
+							{editStatus ? (
+								<div className="edit-box">
+									<button className="edit-btn">
+										<i className="edit-icon" />
+										<span>修改</span>
+									</button>
+								</div>
+							) : (
+								<div className="edit-box">
+									<button className="confirm-btn">保存</button>
+									<button className="cancel-btn">取消</button>
+								</div>
+							)}
+						</div>
+					</li>
+				</ul>
+			</div>
+		</Wrapper>
+	);
+};
 
-  </Wrapper>
-}
-
-export default Main
+export default Main;
