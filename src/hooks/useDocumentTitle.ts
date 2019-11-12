2f583ad75ded33f688b0a871a173b564d89da8e5
anthhub <anthhub@163.com>
2019-11-12 16:04:45 +0800

    feat: mobiPost

--- src/hooks/useDocumentTitle.ts
+++ src/hooks/useDocumentTitle.ts
@@ -1,10 +1,13 @@
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
 
-export default function useDocumentTitle(title: string, defaultTile = 'juejin') {
+export default function useDocumentTitle(title: string) {
+  const titleRef = useRef('')
+
   useEffect(() => {
+    titleRef.current = document.title
     document.title = title
     return () => {
-      document.title = defaultTile
+      document.title = titleRef.current
     }
   }, [])
 }
--- src/routes/lazyComponents.tsx
+++ src/routes/lazyComponents.tsx
@@ -6,20 +6,20 @@ import React, { lazy, Suspense } from 'react'
 export type TLazyComponentsKeys = keyof typeof lazyComponents
 
 const withSuspense = (Component: any) => {
-	return (props: any) => (
-		<Suspense fallback={<SpinCenter />}>
-			<Component {...props} />
-		</Suspense>
-	)
+  return (props: any) => (
+    <Suspense fallback={<SpinCenter />}>
+      <Component {...props} />
+    </Suspense>
+  )
 }
 
 export const lazyComponents = {
-	Frame,
-	Home: withSuspense(lazy(() => import('../pages/home'))),
-	Post: withSuspense(lazy(() => import('../pages/post'))),
-	MobilePost: withSuspense(lazy(() => import('../pages/mobilePost'))),
-	Settings: withSuspense(lazy(() => import('../pages/settings'))),
-	Editor: withSuspense(lazy(() => import('../pages/editor'))),
-	Test: withSuspense(lazy(() => import('../pages/test')))
-	// EditMarkdown: withSuspense(lazy(() => import('../components/EditMarkdown'))),
+  Frame,
+  Home: withSuspense(lazy(() => import('../pages/home'))),
+  Post: withSuspense(lazy(() => import('../pages/post'))),
+  MobilePost: withSuspense(lazy(() => import('../pages/mobiPost'))),
+  Settings: withSuspense(lazy(() => import('../pages/settings'))),
+  Editor: withSuspense(lazy(() => import('../pages/editor'))),
+  Test: withSuspense(lazy(() => import('../pages/test'))),
+  // EditMarkdown: withSuspense(lazy(() => import('../components/EditMarkdown'))),
 }
