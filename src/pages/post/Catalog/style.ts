4d299eae5a1947a6b8931e21d829df064723d3cf
svvhuang <svveetiehuang@163.com>
2019-11-20 15:58:17 +0800

    catalog css

--- src/pages/post/Catalog/style.ts
+++ src/pages/post/Catalog/style.ts
@@ -30,9 +30,77 @@ export const Wrapper = styled.div`
 		.catalog-body {
 			margin: 6px 0;
 
+			.ant-anchor-link {
+				padding: 0;
+
+				/* 所有链接 */
+				.ant-anchor-link-title {
+					margin: 6px 0;
+					padding: 4px 0 4px 21px;
+					color: #000;
+					::before {
+						content: '';
+						position: absolute;
+						top: 50%;
+						transform: translateY(-50%);
+						z-index: 1000;
+						width: 4px;
+						height: 4px;
+						border-radius: 50%;
+						background: currentColor;
+					}
+				}
+			}
+
+			/* 二級目錄中的鏈接 */
+			.ant-anchor-link > .ant-anchor-link > .ant-anchor-link-title {
+				margin: 0;
+				padding: 4px 0 4px 36px;
+				color: #333;
+
+				::before {
+					left: 24px;
+				}
+			}
+
+			/* 三级目录链接小圆点 */
+			.ant-anchor-link > .ant-anchor-link > .ant-anchor-link > .ant-anchor-link-title ::before {
+				left: 39px;
+			}
+
+			/* 被選中的一級目錄 */
 			.ant-anchor-link-active {
-				color: #007fff;
 				background-color: #ebedef;
+
+				.ant-anchor-link-title-active {
+					color: #007fff;
+				}
+			}
+
+			/* 被選中的二級目錄（考慮優先級） */
+			.ant-anchor-link > .ant-anchor-link-active > .ant-anchor-link-title-active {
+				color: #007fff;
+			}
+
+			/* 鼠標懸停時 */
+			.ant-anchor-link:hover {
+				background: #ebedef;
+
+				.ant-anchor-link-title {
+					/* 如何移除 a:hover 的效果？——把里面的代码全部去掉即可 */
+					/* color: inherit; */
+				}
+			}
+		}
+
+		/* 一级目录中的链接 */
+		.catalog-body > .ant-anchor-link > .ant-anchor-link-title {
+			font-weight: 600;
+
+			::before {
+				left: 5px;
+				width: 6px;
+				height: 6px;
 			}
 		}
 	}
