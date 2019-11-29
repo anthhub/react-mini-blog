5e28c798738f259be3da5ed0cc5eca393e587b47
anthhub <anthhub@163.com>
2019-11-29 15:44:08 +0800

    feat: usePersist

--- src/lib/hooks/usePersist.ts
+++ src/lib/hooks/usePersist.ts
@@ -8,12 +8,6 @@ export function usePersistedContext<T>(context: T, key = 'state', flag = true): 
   const persistedContext = JSON.parse(localStorage.getItem(key) || '') || {}
 
   axios.defaults.headers.common.Authorization = persistedContext!.user && persistedContext.user.access_token
-  console.log(
-    '%c%s',
-    'color: #20bd08;font-size:15px',
-    '===TQY===: usePersistedContext -> persistedContext.user && persistedContext.user.access_token',
-    persistedContext.user && persistedContext.user.access_token
-  )
 
   return persistedContext ? persistedContext : context
 }
@@ -21,11 +15,10 @@ export function usePersistedContext<T>(context: T, key = 'state', flag = true): 
 export function usePersistedReducer([state, dispatch]: any[], key = 'state', flag = true) {
   useEffect(() => {
     axios.defaults.headers.common.Authorization = state.user && state.user.access_token
-    console.log('%c%s', 'color: #20bd08;font-size:15px', '===TQY===: usePersistedReducer -> state.user && state.user.access_token', state.user && state.user.access_token)
 
-    if (!flag) {
-      return
-    }
+    // if (!flag) {
+    //   return
+    // }
     return localStorage.setItem(key, JSON.stringify(state))
   }, [state])
   return [state, dispatch]
